87937b39b031c9c5f66ae3907a6c6ffafb9fff15
Everton Paulino <evertonpaulino88@gmail.com>
2021-12-15 21:12:01 -0400

    Test library

--- script-teste.R
+++ script-teste.R
@@ -8,4 +8,14 @@ join()
 select()
 
 #test pull
-list <- c("Ok","Blz")
+obs <- c("obs1","obs2", "obs3", "obs4", "obs5")
+obs
+
+col1 <- (1:5)
+col2 <- c(F,T,F,F,T)
+
+library(tibble)
+
+table1 <- tibble(var1 = obs,
+                 var2 = col1,
+                 var3 = col2)
